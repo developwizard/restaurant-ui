51edc88d81759418a58547583b2a70428db5c186
dewiz <churkin.it@gmail.com>
2023-05-27 21:24:22 +0300

    Added bootstrap modal

--- src/app/restaurant-dashboard/restaurant-dashboard.component.ts
+++ src/app/restaurant-dashboard/restaurant-dashboard.component.ts
@@ -1,12 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, TemplateRef} from '@angular/core';
+import {BsModalRef, BsModalService, ModalModule} from "ngx-bootstrap/modal";
 
 @Component({
   selector: 'app-restaurant-dashboard',
   templateUrl: './restaurant-dashboard.component.html',
   styleUrls: ['./restaurant-dashboard.component.css'],
   standalone: true,
-  imports: []
+  imports: [ModalModule]
 })
 export class RestaurantDashboardComponent {
+  modalRef?: BsModalRef;
 
+  constructor(private modalService: BsModalService) {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
