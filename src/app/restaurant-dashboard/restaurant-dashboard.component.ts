3d6e3b0f9fcfc85b0e0f12aea815c32afda2bdc7
dewiz <churkin.it@gmail.com>
2023-05-27 21:42:28 +0300

    Added restaurant reactive form

--- src/app/restaurant-dashboard/restaurant-dashboard.component.ts
+++ src/app/restaurant-dashboard/restaurant-dashboard.component.ts
@@ -1,19 +1,31 @@
-import {Component, TemplateRef} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {BsModalRef, BsModalService, ModalModule} from "ngx-bootstrap/modal";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-restaurant-dashboard',
   templateUrl: './restaurant-dashboard.component.html',
   styleUrls: ['./restaurant-dashboard.component.css'],
   standalone: true,
-  imports: [ModalModule]
+  imports: [ModalModule, ReactiveFormsModule]
 })
-export class RestaurantDashboardComponent {
+export class RestaurantDashboardComponent implements OnInit {
   modalRef?: BsModalRef;
+  formValue!: FormGroup;
 
-  constructor(private modalService: BsModalService) {}
+  constructor(private modalService: BsModalService, private formBuilder: FormBuilder) {}
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
+  }
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: ['']
+    });
   }
 }
