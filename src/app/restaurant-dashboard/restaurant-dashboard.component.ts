80588792dd04e1c726256ec625492a56d63ae6c9
dewiz <churkin.it@gmail.com>
2023-05-27 23:01:45 +0300

    Implemented fetch all restaurants inside table

--- src/app/restaurant-dashboard/restaurant-dashboard.component.ts
+++ src/app/restaurant-dashboard/restaurant-dashboard.component.ts
@@ -1,19 +1,31 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
 import {BsModalRef, BsModalService, ModalModule} from "ngx-bootstrap/modal";
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {ApiService} from "../shared/api";
+import {RestaurantData} from "./restaurant.model";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-restaurant-dashboard',
   templateUrl: './restaurant-dashboard.component.html',
   styleUrls: ['./restaurant-dashboard.component.css'],
   standalone: true,
-  imports: [ModalModule, ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    ModalModule,
+    ReactiveFormsModule
+  ]
 })
 export class RestaurantDashboardComponent implements OnInit {
   modalRef?: BsModalRef;
   formValue!: FormGroup;
+  restaurantModelObj: RestaurantData = new RestaurantData();
+  allRestaurantsData: RestaurantData[] = [];
 
-  constructor(private modalService: BsModalService, private formBuilder: FormBuilder) {}
+  constructor(private modalService: BsModalService,
+              private formBuilder: FormBuilder,
+              private api: ApiService) {
+  }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
@@ -27,5 +39,12 @@ export class RestaurantDashboardComponent implements OnInit {
       address: [''],
       services: ['']
     });
+    this.getAllData()
+  }
+
+  getAllData() {
+    this.api.getRestaurant().subscribe((res: RestaurantData[]) => {
+      this.allRestaurantsData = res;
+    })
   }
 }
